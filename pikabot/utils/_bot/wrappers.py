f9223dae3367bc9fb18086466a1f884b37df5e0d
Sachin Jangir <support@ItzSjDude.in>
2021-12-26 22:07:52 +0530

    Update wrappers.py

--- pikabot/utils/_bot/wrappers.py
+++ pikabot/utils/_bot/wrappers.py
@@ -9,31 +9,31 @@ TGBOT_USERS = set(
 # ©ItzSjDude
 
 
-def pikatgbot(pika=None, silent=None):
+def infxtgbot(infx=None, silent=None):
     def decorator(func):
         @wraps(func)
         async def wrapper(event):
-            _selfpika = await tgbot.get_me()
-            if "AdmOnly" in pika:
-                _pika = await tgbot.get_permissions(int(event.chat_id), event.sender_id)
-                if _pika.is_admin:
+            _selfinfx = await tgbot.get_me()
+            if "AdmOnly" in infx:
+                _infx = await tgbot.get_permissions(int(event.chat_id), event.sender_id)
+                if _infx.is_admin:
                     await func(event)
                 if event.sender_id == bot.uid:
                     pass
-                if not _pika.is_admin:
+                if not _infx.is_admin:
                     if silent is None:
                         await event.reply("You need to be admin to use this command")
 
-            if "AmIAdm" in pika:
+            if "AmIAdm" in infx:
 
-                _pika = await tgbot.get_permissions(int(event.chat_id), _selfpika.id)
-                if _pika.is_admin:
+                _infx = await tgbot.get_permissions(int(event.chat_id), _selfinfx.id)
+                if _infx.is_admin:
                     await func(event)
                 else:
                     if silent is None:
                         await event.reply("I am not Admin Nibba😷")
 
-            if "OwnSudo" in pika:
+            if "OwnSudo" in infx:
                 tgbotusers = list(TGBOT_USERS)
                 if event.sender_id == bot.uid or event.sender_id in tgbotusers:
                     await func(event)
@@ -41,14 +41,14 @@ def pikatgbot(pika=None, silent=None):
                     if silent is None:
                         await event.reply("**Error**: You are not a Sudo User, Owner.")
 
-            if "Owner" in pika:
+            if "Owner" in infx:
                 if event.sender_id == bot.uid:
                     await func(event)
                 else:
                     if silent is None:
                         await event.reply("Only Owners can execute this Cmd")
 
-            if "BotSudo" in pika:
+            if "BotSudo" in infx:
                 if event.sender_id in list(TGBOT_USERS):
                     await func(event)
 
