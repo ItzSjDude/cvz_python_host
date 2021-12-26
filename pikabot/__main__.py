211a498e34ea86da985648106df225a831eea31f
Sachin Jangir <support@ItzSjDude.in>
2021-12-26 22:03:15 +0530

    Update __main__.py

--- pikabot/__main__.py
+++ pikabot/__main__.py
@@ -9,8 +9,8 @@
 #
 # All rights reserved
 
-from . import pikabot
+from . import infinix
 import asyncio as _asyncio  
 
 if __name__ == "__main__": 
-    _asyncio.get_event_loop().run_until_complete(pikabot())
+    _asyncio.get_event_loop().run_until_complete(infinix())
