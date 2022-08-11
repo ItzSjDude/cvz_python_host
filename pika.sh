3119e01cb8bc0a00293282016ecdec32088e8ae8
Sachin Jangir <support@ItzSjDude.in>
2022-08-11 18:21:01 +0530

    Update pika.sh

--- pika.sh
+++ pika.sh
@@ -75,8 +75,8 @@ apt-get install -y --no-install-recommends \
     policykit-1
 apt autoremove --yes
 
-pip3 install --upgrade pip setuptools && mkdir bin && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && apt install -y ./google-chrome-stable_current_amd64.deb && rm google-chrome-stable_current_amd64.deb && git clone https://github.com/ItzSjDude/infinix.git .  
-pip3 install -r requirements.txt
+git clone https://github.com/ItzSjDude/infinix.git . && pip3 install --upgrade pip setuptools && pip3 install -r requirements.txt && mkdir bin && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && apt install -y ./google-chrome-stable_current_amd64.deb && rm google-chrome-stable_current_amd64.deb
+
 
 
 
