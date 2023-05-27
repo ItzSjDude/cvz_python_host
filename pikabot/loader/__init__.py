3f757611942395e38035081d234769ff637f47e8
mightycatx <134790812+mightycatx@users.noreply.github.com>
2023-05-28 02:47:46 +0530

    Update __init__.py

--- pikabot/loader/__init__.py
+++ pikabot/loader/__init__.py
@@ -38,7 +38,7 @@ def pika_plugins(_pikamod=None):
     spec = spec_from_file_location(name, path)
     _pika = module_from_spec(spec)
                                    #____Pikabot__Plugins__Loader____
-    infinix = userbot = pikabot; _pika.bot = bot; _pika.Var = Var; _pika.rx = rx; _pika.Infinix = ItzSjDude; _pika.ItzSjDude = ItzSjDude; _pika.pdb = pdb; _pika.Config = Var; _pika.borg = bot; _pika.logger = logging.getLogger(_pikamod)
+    INFINIX = userbot = pikabot; _pika.bot = bot; _pika.Var = Var; _pika.rx = rx; _pika.Infinix = ItzSjDude; _pika.ItzSjDude = ItzSjDude; _pika.pdb = pdb; _pika.Config = Var; _pika.borg = bot; _pika.logger = logging.getLogger(_pikamod)
     modules["userbot"] = pikabot; modules["userbot.utils"] = _utilz; modules["infinix"] = pikabot ; spec.loader.exec_module(_pika); bot.pika_cmd[_pikamod] = _pika; modules["pikabot"+_pikamod] = _pika; logpl.info("🔥Imported "+_pikamod)
 
 def load_ext_module(shortname):
