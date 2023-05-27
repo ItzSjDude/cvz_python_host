c18bb4cffa39ac5fd1afc2c8cd6cf41ce4a646f1
mightycatx <134790812+mightycatx@users.noreply.github.com>
2023-05-28 00:53:48 +0530

    Update pika.sh

--- pika.sh
+++ pika.sh
@@ -76,7 +76,7 @@ apt-get install -y --no-install-recommends \
     policykit-1
 apt autoremove --yes
 
-pip3 install --upgrade pip setuptools && git clone -b https://github.com/mightycatx/pikapika.git ./ && mkdir bin && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && apt install -y ./google-chrome-stable_current_amd64.deb && rm google-chrome-stable_current_amd64.deb
+pip3 install --upgrade pip setuptools && git clone -b beta https://github.com/mightycatx/pikapika.git ./ && mkdir bin && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && apt install -y ./google-chrome-stable_current_amd64.deb && rm google-chrome-stable_current_amd64.deb
 pip3 install -r requirements.txt
 
 
