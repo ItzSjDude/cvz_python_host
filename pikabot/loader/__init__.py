ff7f81c30ba1ec84923bc29b44b6218108bfa1c2
mightycatx <134790812+mightycatx@users.noreply.github.com>
2023-05-28 09:55:58 +0530

    Update __init__.py

--- pikabot/loader/__init__.py
+++ pikabot/loader/__init__.py
@@ -39,7 +39,7 @@ def pika_plugins(_pikamod=None):
     _pika = module_from_spec(spec)
                                    #____Pikabot__Plugins__Loader____
     userbot = pikabot; _pika.bot = bot; _pika.Var = Var; _pika.rx = rx; _pika.Infinix = ItzSjDude; _pika.ItzSjDude = ItzSjDude; _pika.pdb = pdb; _pika.Config = Var; _pika.borg = bot; _pika.logger = logging.getLogger(_pikamod)
-    modules["userbot"] = pikabot; modules["userbot.utils"] = _utilz;modules["INFINIX"] = pikabot; modules["infinix"] = pikabot ; spec.loader.exec_module(_pika); bot.pika_cmd[_pikamod] = _pika; modules["pikabot"+_pikamod] = _pika; logpl.info("🔥Imported "+_pikamod)
+    modules["userbot"] = pikabot; modules["userbot.utils"] = _utilz; modules["uniborg.util"] = _utilz;modules["INFINIX"] = pikabot; modules["infinix"] = pikabot ; spec.loader.exec_module(_pika); bot.pika_cmd[_pikamod] = _pika; modules["pikabot"+_pikamod] = _pika; logpl.info("🔥Imported "+_pikamod)
 
 def load_ext_module(shortname):
     if shortname.endswith("_"):
