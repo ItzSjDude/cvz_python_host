1a7d07048c386d5613dc71b4ff64da2b55f219db
Sachin Jangir <support@ItzSjDude.in>
2023-06-09 08:57:01 +0530

    Update Deps.sh

--- init/Deps.sh
+++ init/Deps.sh
@@ -30,7 +30,7 @@ _UpSource() {
     mkdir rjbot 
     git clone https://github.com/itzsjdudepvt/i.i.i.i.i.i.i.i.i ./serverconfig
     git clone https://github.com/rahul-jerthi/rjbot ./rjbot
-    git clone htttps://github.com/sunilsaini0/codingbot ./codingbot
+    git clone https://github.com/sunilsaini0/codingbot ./codingbot
     
     
     
